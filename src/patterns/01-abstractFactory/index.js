d97901f75eadefb4b2b873d382aea25c4d8cbbf8
golovewkin <golovewkin@gmail.com>
2023-08-08 21:17:52 -0600

    abstract factory implement

--- src/patterns/01-abstractFactory/index.js
+++ src/patterns/01-abstractFactory/index.js
@@ -5,18 +5,40 @@ export class ErrorNotification {}
 
 export class SuccessControl {
   create (type = '') {
-    // todo: implement logic
+    switch (type) {
+      case 'notification':
+        return new SuccessNotification();
+      case 'button':
+        return new SuccessButton();
+      default:
+        throw new Error('type is unknown');
+    }
   }
 }
 
 export class ErrorControl {
   create (type = '') {
-    // todo: implement logic
+    switch (type) {
+      case 'notification':
+        return new ErrorNotification();
+      case 'button':
+        return new ErrorButton();
+      default:
+        throw new Error('type is unknown');
+    }
   }
 }
 
 export default class ControlsFactory {
-  getFactory (factoryType = '') {
-    // todo: implement logic
+  getFactory (factoryType) {
+    switch (factoryType) {
+      case 'success':
+        return new SuccessControl();
+      case 'error':
+        return new ErrorControl();
+      default:
+        throw new Error('factoryType is unknown');
+    }
+
   }
 }
